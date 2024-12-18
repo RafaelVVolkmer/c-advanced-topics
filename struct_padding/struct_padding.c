e16254b80d89488c3f661c4be835570f5ba48143
RafaelVolkmer <Rafael.V.Volkmer@gmail.com>
2024-12-17 21:12:58 -0300

    Update struct_padding.c

--- struct_padding/struct_padding.c
+++ struct_padding/struct_padding.c
@@ -4,17 +4,28 @@
 
 typedef struct paddingStruct 
 {
-    int  element_0;  // 4-bytes
-    char element_1;  // 1-byte (var) + 3-bytes (padding)
-    int  element_2;  // 4-bytes
+    int  element_0;  /*< 4-bytes >*/
+    char element_1;  /*< 1-byte (var) + 3-bytes (padding) >*/
+    int  element_2;  /*< 4-bytes >*/
 } padding_t;
 
 typedef struct __attribute__((packed)) packedStruct
 {
-    int  element_0;  // 4-bytes
-    char element_1;  // 1-byte
-    int  element_2;  // 4-bytes
+    int  element_0;  /*< 4-bytes >*/
+    char element_1;  /*< 1-byte  >*/
+    int  element_2;  /*< 4-bytes >*/
 } packed_t;
+
+#pragma pack(push, 1u)
+
+typedef struct pragmaStruct
+{
+    int  element_0;  /*< 4-bytes >*/
+    char element_1;  /*< 1-byte  >*/
+    int  element_2;  /*< 4-bytes >*/
+} pragma_t;
+
+#pragma pack(pop)
 
 static int memDumpStruct(const void *data, size_t size);
 
@@ -22,6 +33,7 @@ int main(void)
 {
     padding_t struct_0;
     packed_t  struct_1;
+    pragma_t  struct_2;
 
     struct_0.element_0 = 0x11223344;
     struct_0.element_1 = 0x55;
@@ -30,7 +42,12 @@ int main(void)
     struct_1.element_0 = 0x11223344;
     struct_1.element_1 = 0x55;
     struct_1.element_2 = 0xAABBCCDD;
-
+    
+    struct_2.element_0 = 0x11223344;
+    struct_2.element_1 = 0x55;
+    struct_2.element_2 = 0xAABBCCDD;
+    
+    printf("\n");
     printf("=== Padding Struct ===\n");
     printf("struct_0.element_0 = 0x%08X\n", struct_0.element_0);
     printf("struct_0.element_1 = 0x%02X\n", (unsigned char)struct_0.element_1);
@@ -38,7 +55,8 @@ int main(void)
     printf("\n");
     
     memDumpStruct(&struct_0, sizeof(struct_0));
-
+    
+    printf("\n");
     printf("\n=== Packed Struct ===\n");
     printf("struct_1.element_0 = 0x%08X\n", struct_1.element_0);
     printf("struct_1.element_1 = 0x%02X\n", (unsigned char)struct_1.element_1);
@@ -46,6 +64,15 @@ int main(void)
     printf("\n");
     
     memDumpStruct(&struct_1, sizeof(struct_1));
+    
+    printf("\n");
+    printf("=== Pragma Struct ===\n");
+    printf("struct_0.element_0 = 0x%08X\n", struct_2.element_0);
+    printf("struct_0.element_1 = 0x%02X\n", (unsigned char)struct_2.element_1);
+    printf("struct_0.element_2 = 0x%08X\n", struct_2.element_2);
+    printf("\n");
+    
+    memDumpStruct(&struct_2, sizeof(struct_2));
 
     return 0;
 }
